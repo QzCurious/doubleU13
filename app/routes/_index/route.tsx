45174b82b7958f265172c3535bdb44c964bca9ab
Qz <qzcurious@gmail.com>
2026-01-03 14:29:42 +0800

    load screen

--- app/routes/_index/route.tsx
+++ app/routes/_index/route.tsx
@@ -1,4 +1,13 @@
-export default function Home() {
+export default function Route() {
+  return (
+    <div>
+      <Colors />
+      <LoadScreen />
+    </div>
+  );
+}
+
+function Colors() {
   return (
     <section className="space-y-2 border border-gray-400 p-4">
       <h2>Colors</h2>
@@ -15,5 +24,28 @@ export default function Home() {
         <span>secondary</span>
       </div>
     </section>
+  );
+}
+
+function LoadScreen() {
+  return (
+    <div className="bg-dark-blue relative flex min-h-screen flex-col p-8">
+      <div className="text-white">
+        <h1 className="text-8xl">doubleU</h1>
+        <p className="mt-4 text-xl">Explore & Build Cool Sites</p>
+      </div>
+
+      <div className="mt-auto">
+        <p className="text-2xl text-white">
+          <span className="text-5xl">00</span>
+          <span className="text-xl">%</span>
+        </p>
+
+        <div className="relative mt-4 h-px">
+          <div className="absolute inset-0 bg-white/50"></div>
+          <div className="bg-secondary absolute inset-0 w-1/2"></div>
+        </div>
+      </div>
+    </div>
   );
 }
