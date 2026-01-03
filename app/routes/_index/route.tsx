c4ee917ce321105e9c040afcb1a5d26057ab04de
Qz <qzcurious@gmail.com>
2026-01-03 14:12:03 +0800

    site title

--- app/routes/_index/route.tsx
+++ app/routes/_index/route.tsx
@@ -1,12 +1,3 @@
-import type { Route } from "./+types/route";
-
-export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
-}
-
 export default function Home() {
   return (
     <section className="border border-gray-400 space-y-2 p-4">
