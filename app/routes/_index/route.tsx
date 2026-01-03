d1603a1dfb9c5f60afb80759c67ec5044eb60e65
Qz <qzcurious@gmail.com>
2026-01-03 13:58:41 +0800

    colors

--- app/routes/_index/route.tsx
+++ app/routes/_index/route.tsx
@@ -8,5 +8,21 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return "home";
+  return (
+    <section className="border border-gray-400 space-y-2 p-4">
+      <h2>Colors</h2>
+      <div className="flex items-center gap-2">
+        <div className="size-5 bg-primary"></div>
+        <span>primary</span>
+      </div>
+      <div className="flex items-center gap-2">
+        <div className="size-5 bg-dark-blue"></div>
+        <span>dark-blue</span>
+      </div>
+      <div className="flex items-center gap-2">
+        <div className="size-5 bg-secondary"></div>
+        <span>secondary</span>
+      </div>
+    </section>
+  );
 }
