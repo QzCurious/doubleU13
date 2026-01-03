f0e60bedb117e8238d30ebbb7b45b3fb7aae0cfe
Qz <qzcurious@gmail.com>
2026-01-03 14:21:31 +0800

    prettier

--- app/routes/_index/route.tsx
+++ app/routes/_index/route.tsx
@@ -1,17 +1,17 @@
 export default function Home() {
   return (
-    <section className="border border-gray-400 space-y-2 p-4">
+    <section className="space-y-2 border border-gray-400 p-4">
       <h2>Colors</h2>
       <div className="flex items-center gap-2">
-        <div className="size-5 bg-primary"></div>
+        <div className="bg-primary size-5"></div>
         <span>primary</span>
       </div>
       <div className="flex items-center gap-2">
-        <div className="size-5 bg-dark-blue"></div>
+        <div className="bg-dark-blue size-5"></div>
         <span>dark-blue</span>
       </div>
       <div className="flex items-center gap-2">
-        <div className="size-5 bg-secondary"></div>
+        <div className="bg-secondary size-5"></div>
         <span>secondary</span>
       </div>
     </section>
